360b6c1f340648a88e57c156e73c37906f1dbca9
DerekMA15 <derekmadev@gmail.com>
2026-06-01 11:55:58 -0300

    feat: class model Paciente

--- src/main/java/model/Paciente.java
+++ src/main/java/model/Paciente.java
@@ -1,5 +1,52 @@
 package model;
 
 public class Paciente {
+    String nome; 
+    String cpf; 
+    int prioridade; // (na escala de 1 a 5)
+    int idade; // seria algo pego diretamente no cpf, mas para facilitar o nosso caso né
+    boolean idoso = false; // acho que ficou meio redundante por conta da idade, mas fica melhor trabalhar com o boolean
     
+    public Paciente(String nome, String cpf, int prioridade, int idade) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.prioridade = prioridade;
+        this.idade = idade;
+        if (idade >= 60) {
+            setIdoso(true);
+    }
+
+    }
+    public String getNome() {
+        return nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public int getPrioridade() {
+        return prioridade;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    
+    public boolean isIdoso() {
+        return idoso;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public void setIdoso(boolean idoso) {
+        this.idoso = idoso;
+    }
 }
