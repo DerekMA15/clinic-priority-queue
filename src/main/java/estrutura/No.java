c44f5f96d19b14a56ef8ba90230632a0d25b84ff
DerekMA15 <derekmadev@gmail.com>
2026-06-01 13:13:35 -0300

    fix: ajuste para que conseguisse se comunicar diretamente a classe Paciente

--- src/main/java/estrutura/No.java
+++ src/main/java/estrutura/No.java
@@ -3,24 +3,25 @@ package estrutura;
 import model.Paciente;
 
 public class No {
-    String nome;
     Paciente paciente; 
     No prev; // no anterior
     No next; // proximo no 
 
 
     //inserirOrdenado(Paciente p)
-    No(String n, Paciente p, No ant, No prxm){
-        nome = n;
+    No(Paciente p, No ant, No prxm){
         paciente = p;
         prev = ant;
         next = prxm; 
     }
 
-    No(String n){
-        this(n,null,null, null); // revisar se é a melhor maneira, pq o paciente vai entrar nulo tbm(embora que essa No só seja usado como auxiliar)
+    No(Paciente p){
+        this(p,null, null); // revisar se é a melhor maneira, pq o paciente vai entrar nulo tbm(embora que essa No só seja usado como auxiliar)
     }
 
+    public String getPaciente(){
+        return paciente.getNome();
+    }
     
 }
 
